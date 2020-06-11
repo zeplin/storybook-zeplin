d0a2b048e4fe57e7ef0f6e4af65258a776def57e
mertkahyaoglu <mertkahyaoglu93@gmail.com>
2020-06-11 17:00:05 +0300

    Add component docs

--- .storybook/Button/Button.jsx
+++ .storybook/Button/Button.jsx
@@ -3,6 +3,16 @@ import PropTypes from "prop-types";
 
 import "./style.css";
 
+/**
+ * *Default* button used across the application.
+ *
+ * | Props    | Types       |
+ * |----------|------------:|
+ * | active   | boolean     |
+ * | children | JSX.Element |
+ *
+ * @param {Object} props
+ */
 export default function Button({ active, children }) {
     return <button className={active ? "active" : ""}>{children}</button>;
 }
