38616c6ef60e678156b6a96ac4f4ac5377caa12c
Sertaç Karahoda <skarahoda@sabanciuniv.edu>
2021-10-12 10:39:15 +0300

    Change parent origin with beta

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,8 @@ export const PANEL_ID = `${ADDON_ID}/panel`;
 
 export const {
     STORYBOOK_ZEPLIN_TOKEN: ZEPLIN_TOKEN,
-    STORYBOOK_ZEPLIN_PARENT_ORIGIN: PARENT_ORIGIN = "https://app.zeplin.io",
+    // TODO: replace beta url with https://app.zeplin.io before release
+    STORYBOOK_ZEPLIN_PARENT_ORIGIN: PARENT_ORIGIN = "https://beta.zeplin.io",
     STORYBOOK_ZEPLIN_API_URL: ZEPLIN_API_URL = "https://api.zeplin.dev/v1",
     STORYBOOK_ZEPLIN_WEB_BASE: ZEPLIN_WEB_BASE = "https://app.zeplin.io",
     STORYBOOK_ZEPLIN_APP_BASE: ZEPLIN_APP_BASE = "zpl:"
