ceceb23c7772c3338115ca378ffd762007d6d282
Younho Choo <yo+github@younho9.com>
2022-02-21 16:12:08 +0900

    fix: parse process.env properly (#48)
    * fix: parse process.env properly
    
    * chore: delete yarn.lock

--- src/constants.ts
+++ src/constants.ts
@@ -3,10 +3,8 @@ export const ADDON_ID = "zeplin";
 export const PARAM_KEY = "zeplinLink";
 export const PANEL_ID = `${ADDON_ID}/panel`;
 
-export const {
-    STORYBOOK_ZEPLIN_TOKEN: ZEPLIN_TOKEN,
-    STORYBOOK_ZEPLIN_PARENT_ORIGIN: PARENT_ORIGIN = "https://app.zeplin.io",
-    STORYBOOK_ZEPLIN_API_URL: ZEPLIN_API_URL = "https://api.zeplin.dev",
-    STORYBOOK_ZEPLIN_WEB_BASE: ZEPLIN_WEB_BASE = "https://app.zeplin.io",
-    STORYBOOK_ZEPLIN_APP_BASE: ZEPLIN_APP_BASE = "zpl:"
-} = process.env;
+export const ZEPLIN_TOKEN = process.env.STORYBOOK_ZEPLIN_TOKEN;
+export const PARENT_ORIGIN = process.env.STORYBOOK_ZEPLIN_PARENT_ORIGIN ?? "https://app.zeplin.io";
+export const ZEPLIN_API_URL = process.env.STORYBOOK_ZEPLIN_API_URL ?? "https://api.zeplin.dev";
+export const ZEPLIN_WEB_BASE = process.env.STORYBOOK_ZEPLIN_WEB_BASE ?? "https://app.zeplin.io";
+export const ZEPLIN_APP_BASE = process.env.STORYBOOK_ZEPLIN_APP_BASE ?? "zpl:";
