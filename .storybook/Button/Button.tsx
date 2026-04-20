3f19db1521591c996b2d6c9b99f31cb220fb70c0
Berk Çebi <berk@zeplin.io>
2026-04-20 11:43:35 -0500

    Address review comments

--- .storybook/Button/Button.tsx
+++ .storybook/Button/Button.tsx
@@ -1,13 +1,16 @@
-import { ReactNode } from "react";
+import { PropsWithChildren } from "react";
 import "./style.css";
 
 interface ButtonProps {
     active?: boolean;
     disabled?: boolean;
-    children?: ReactNode;
 }
 
-export default function Button({ active, disabled, children }: ButtonProps) {
+export default function Button({
+    active,
+    disabled,
+    children,
+}: PropsWithChildren<ButtonProps>) {
     return (
         <button className={active ? "active" : ""} disabled={disabled}>
             {children}
